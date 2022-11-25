a5e5df8313e1a9080535b7ec03939d50b516b689
Mailson Moreira <67443036+MirsonSM@users.noreply.github.com>
2022-11-25 19:24:25 -0300

    inserção footer e main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+document.querySelector('.recentPost h1').addEventListener('click', () => {
+console.log('clicado')
+})
+
+
+document.querySelector('.projects p').addEventListener('click', () => {
+    console.log('clicado')
+})
